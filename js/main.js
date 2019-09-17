3ce46f9e5397c13ed6f5b43cc01c9ac7d1de6a6c
Salvador Hernández López <chavatloz@gmail.com>
2019-09-16 21:22:48 -0500

    smoothScroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const facebook = document.querySelector('.facebook-circle');
     const twitter = document.querySelector('.twitter-circle');
     const instagram = document.querySelector('.instagram-circle');
     const github = document.querySelector('.github-circle');
-    let socialNetworks = [facebook,twitter,instagram,github];
-    let URLs = ['https://www.facebook.com/ChavaTI','https://twitter.com/ChavaTI_','https://www.instagram.com/chavatloz/','https://github.com/ChavaTI'];
+    let socialNetworks = [facebook, twitter, instagram, github];
+    let URLs = ['https://www.facebook.com/ChavaTI', 'https://twitter.com/ChavaTI_', 'https://www.instagram.com/chavatloz/', 'https://github.com/ChavaTI'];
 
 
     for (let i = 0; i < socialNetworks.length; i++) {
@@ -19,7 +19,55 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
         element.addEventListener('click', () => {
-            window.open(url,'_blank');
-        });    
+            window.open(url, '_blank');
+        });
     }
- });
+});
+
+
+const ease = (t, b, c, d) => {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+}
+
+const smoothScroll = (targetId, duration) => {
+    let target = document.querySelector(targetId);
+    let targetPosition = target.getBoundingClientRect().top;
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+
+    const animation = currentTime => {
+        if (startTime === null) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+
+
+        let run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    requestAnimationFrame(animation);
+}
+
+document.querySelector('#toAbout').addEventListener('click', () => {
+    smoothScroll('#about',2000);
+});
+
+document.querySelector('#toSkills').addEventListener('click', () => {
+    smoothScroll('#skills',2000);
+});
+
+document.querySelector('#toPortafolio').addEventListener('click', () => {
+    smoothScroll('#portafolio',2000);
+});
+
+document.querySelector('#toExperience').addEventListener('click', () => {
+    smoothScroll('#experience',2000);
+});
+
+document.querySelector('#toContact').addEventListener('click', () => {
+    smoothScroll('#contact',2000);
+});
