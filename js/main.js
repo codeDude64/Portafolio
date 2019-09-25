6f68e27e2acca74cfb19ca2b6703388919729b28
Salvador Hernández López <chavatloz@gmail.com>
2019-09-24 19:29:08 -0500

    check and uncheck skill

--- js/main.js
+++ js/main.js
@@ -6,21 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const instagram = document.querySelector('.instagram-circle');
     const github = document.querySelector('.github-circle');
     const linkedin = document.querySelector('.linkedin-circle');
-    let socialNetworks = [facebook, twitter, instagram, github,linkedin];
-    let URLs = ['https://www.facebook.com/ChavaTI', 'https://twitter.com/ChavaTI_', 'https://www.instagram.com/chavatloz/', 'https://github.com/ChavaTI','https://www.linkedin.com/in/salvador-hernández-lópez-a2ab7315a/'];
+    let socialNetworks = [facebook, twitter, instagram, github, linkedin];
+    let URLs = ['https://www.facebook.com/ChavaTI', 'https://twitter.com/ChavaTI_', 'https://www.instagram.com/chavatloz/', 'https://github.com/ChavaTI', 'https://www.linkedin.com/in/salvador-hernández-lópez-a2ab7315a/'];
 
 
     for (let i = 0; i < socialNetworks.length; i++) {
         const element = socialNetworks[i];
         const url = URLs[i];
-        element.addEventListener('mouseover', () => {
-            document.body.style.cursor = "pointer";
-        });
-
-        element.addEventListener('mouseout', () => {
-            document.body.style.cursor = "default";
-        });
-
         element.addEventListener('click', () => {
             window.open(url, '_blank');
         });
@@ -57,19 +49,19 @@ const smoothScroll = (targetId, duration) => {
 }
 
 const menuItems = [document.querySelector('#toAbout'),
-                  document.querySelector('#toSkills'),
-                  document.querySelector('#toPortafolio'),
-                  document.querySelector('#toExperience'),
-                  document.querySelector('#toContact')
-                 ];
-const targets = ['#about','#skills','#portafolio','#experience','#contact'];
+    document.querySelector('#toSkills'),
+    document.querySelector('#toPortafolio'),
+    document.querySelector('#toExperience'),
+    document.querySelector('#toContact')
+];
+const targets = ['#about', '#skills', '#portafolio', '#experience', '#contact'];
 
 for (let i = 0; i < menuItems.length; i++) {
     const menuItem = menuItems[i];
     const target = targets[i];
 
     menuItem.addEventListener('click', () => {
-        smoothScroll(target,2000);
+        smoothScroll(target, 2000);
     });
 }
 
@@ -82,86 +74,101 @@ document.querySelector('#react-card').addEventListener('click', () => {
 });
 
 const skills = [
-                document.querySelector('#react-card'),
-                document.querySelector('#angular-card'),
-                document.querySelector('#node-card'),
-                document.querySelector('#php-card'),
-                document.querySelector('#java-card'),
-                document.querySelector('#db-card'),
-                document.querySelector('#linux-card'),
-                document.querySelector('#docker-card'),
-                document.querySelector('#diagram-card'),
-                document.querySelector('#language-card'),
-                document.querySelector('#learning-card')
-               ];
+    document.querySelector('#react'),
+    document.querySelector('#angular'),
+    document.querySelector('#node'),
+    document.querySelector('#php'),
+    document.querySelector('#java'),
+    document.querySelector('#db'),
+    document.querySelector('#linux'),
+    document.querySelector('#docker'),
+    document.querySelector('#diagram'),
+    document.querySelector('#language'),
+    document.querySelector('#learning')
+];
 
 const skillsName = [
-                    'React',
-                    'Angular',
-                    'NodeJS',
-                    'PHP',
-                    'Java',
-                    'Data Bases',
-                    'Linux',
-                    'Docker',
-                    'Software engineering',
-                    'Languages',
-                    'Learning'
-                   ];
+    'React',
+    'Angular',
+    'NodeJS',
+    'PHP',
+    'Java',
+    'Data Bases',
+    'Linux',
+    'Docker',
+    'Software engineering',
+    'Languages',
+    'Learning'
+];
 
 const iconSkill = [
-                    ['fab fa-react','react-card'],
-                    ['fab fa-angular','angular-card'],
-                    ['fab fa-node-js','node-card'],
-                    ['fab fa-php','php-card'],
-                    ['fab fa-java','java-card'],
-                    ['fas fa-database','db-card'],
-                    ['fab fa-linux','linux-card'],
-                    ['fab fa-docker','docker-card'],
-                    ['fas fa-project-diagram','diagram-card'],
-                    ['fas fa-language','language-card'],
-                    ['fas fa-book-reader','learning-card']
-                  ];
+    ['fab fa-react', 'react-card'],
+    ['fab fa-angular', 'angular-card'],
+    ['fab fa-node-js', 'node-card'],
+    ['fab fa-php', 'php-card'],
+    ['fab fa-java', 'java-card'],
+    ['fas fa-database', 'db-card'],
+    ['fab fa-linux', 'linux-card'],
+    ['fab fa-docker', 'docker-card'],
+    ['fas fa-project-diagram', 'diagram-card'],
+    ['fas fa-language', 'language-card'],
+    ['fas fa-book-reader', 'learning-card']
+];
 
 const themes = [
-                '#282c34',
-                '#f0f0f080',
-                '#333333',
-                '#7478ae',
-                '#f13d3d',
-                '#2f5676',
-                '#f0f0f080',
-                '#309aeb',
-                '#1d2f3f',
-                '#87c540',
-                '#3be1f0'
-               ];
+    '#282c34',
+    '#f0f0f080',
+    '#333333',
+    '#7478ae',
+    '#f13d3d',
+    '#2f5676',
+    '#f0f0f080',
+    '#309aeb',
+    '#1d2f3f',
+    '#87c540',
+    '#3be1f0'
+];
+
+
+const checkUncheckRadio = () => {
+    let booRadio;
+    for (let i = 0; i < skills.length; i++) {
+        const element = skills[i];
+
+        element.addEventListener('click', () => {
+
+            if (booRadio == element) {
+                element.checked = false;
+                booRadio = null;
+            } else {
+                booRadio = element;
+            }
+
+
+        });
+    }
+}
+
+checkUncheckRadio();
+
 
 const aboutSkill = document.querySelector('#about-skill');
-
-const skillIsSelected = false;
 
 
 for (let i = 0; i < skills.length; i++) {
     const skill = skills[i];
-    
-    skill.addEventListener('mouseover', () => {
-        document.body.style.cursor = "pointer";
-    });
-
-    skill.addEventListener('mouseout', () => {
-        document.body.style.cursor = "default";
-    });
 
     skill.addEventListener('click', () => {
-        aboutSkill.style.display = 'block';
-        aboutSkill.style.backgroundColor = themes[i];
-        aboutSkill.firstElementChild.firstElementChild.innerHTML = skillsName[i];
-        aboutSkill.firstElementChild.firstElementChild.nextElementSibling.className = iconSkill[i][1] + " card-about-icon" ;
-        aboutSkill.firstElementChild.firstElementChild.nextElementSibling.innerHTML = `<i class = "${iconSkill[i][0]}"></i>`;
 
-       
+        if (skill.checked) {
+            aboutSkill.style.display = 'block';
+            aboutSkill.style.backgroundColor = themes[i];
+            aboutSkill.firstElementChild.firstElementChild.innerHTML = skillsName[i];
+            aboutSkill.firstElementChild.firstElementChild.nextElementSibling.className = iconSkill[i][1] + " card-about-icon";
+            aboutSkill.firstElementChild.firstElementChild.nextElementSibling.innerHTML = `<i class = "${iconSkill[i][0]}"></i>`;
+        } else {
+            aboutSkill.style.display = 'none';
+        }
     });
-    
-}
 
+}
